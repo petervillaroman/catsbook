746bb55c2b4cedc7a9ef270d69ce1d05ed3caed8
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-06-30 12:12:22 -0700

    BODY MARGINS FIXED! // Responsive AppBar Elements
    We finally know how to switch the margins to zero whenever we implement our app bar.
    
    We made new considerations for mobile.
    
    K.P. Villaroman turns into a small Icon whenever we resize our browser to mobile dimensions.
    
    COOL STUFF!! : )

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,28 @@
 /** @format */
-
+import AirlineSeatLegroomReducedIcon from '@mui/icons-material/AirlineSeatLegroomReduced';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { styled } from '@mui/material';
+import { Block } from '@mui/icons-material';
+
+const StyledToolbar = styled(Toolbar)({
+	display: 'flex',
+	justifyContent: 'space-between',
+});
 
 export const NavBar = () => {
-	return <div>NavBar</div>;
+	return (
+		<AppBar position='sticky'>
+			<StyledToolbar>
+				<Typography
+					variant='h6'
+					sx={{ display: { xs: 'none', sm: 'block' } }}>
+					KP Villaroman
+				</Typography>
+				<AirlineSeatLegroomReducedIcon
+					sx={{ display: { xs: 'block', sm: 'none' } }}
+				/>
+			</StyledToolbar>
+		</AppBar>
+	);
 };
