450b877e8302585ceb9f5d3f94c9f480b691811b
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-01 10:49:27 -0700

    FAB now repositions on resize
    The FAB is now responsive to resize. When it's small or lower it shows up  on the bottom center of the screen.

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -6,7 +6,13 @@ import { Add as AddIcon } from '@mui/icons-material';
 function Add() {
 	return (
 		<>
-			<Tooltip title='Delete' sx={{ position: 'fixed', bottom: 20 }}>
+			<Tooltip
+				title='Delete'
+				sx={{
+					position: 'fixed',
+					bottom: 20,
+					left: { xs: 'calc(50% - 25px)', md: 30 },
+				}}>
 				<Fab color='primary' aria-label='add'>
 					<AddIcon />
 				</Fab>
