a388008734f0ef544ea05827026e2f2b6b6b9bf9
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-01 11:26:05 -0700

    Styled Modal/Box components
    We created custom versions of Modal and Box named StyledModal and UserBox.
    
    This allows us to apply css to our components without cluttering up the function return too much.
    
    This also allows us to reuse our components if we need to.
    
    These are the two primary benefits of modularizing your code. : )

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,25 @@
 /** @format */
 
-import { Box, Fab, Modal, Tooltip, Typography } from '@mui/material';
+import { Box, Fab, Modal, styled, Tooltip } from '@mui/material';
 import { React, useState } from 'react';
 import { Add as AddIcon } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+import { Avatar } from '@mui/material';
 function Add() {
 	const [open, setOpen] = useState(false);
 
+	const StyledModal = styled(Modal)({
+		display: 'flex',
+		alignItem: 'center',
+		justifyContent: 'center',
+	});
+
+	const UserBox = styled(Box)({
+		display: 'flex',
+		alignItem: 'center',
+		gap: '10px',
+		marginBottom: '20px',
+	});
 	return (
 		<>
 			<Tooltip
@@ -20,20 +34,28 @@ function Add() {
 					<AddIcon />
 				</Fab>
 			</Tooltip>
-			<Modal
+			<StyledModal
 				open={open}
 				onClose={(e) => setOpen(false)}
 				aria-labelledby='modal-modal-title'
 				aria-describedby='modal-modal-description'>
 				<Box
-					width={100}
-					height={100}
+					width={400}
+					height={280}
 					bgcolor='white'
 					p={2}
 					borderRadius={5}>
-					Hello{' '}
+					<Typography variant='h6' color={'gray'} textAlign='center'>
+						Create post
+					</Typography>
+					<UserBox>
+						<Avatar
+							src='https://i.imgur.com/M2UFQ68.jpg'
+							sx={{ width: 30, height: 30 }}></Avatar>
+						<Typography>KP Villaroman</Typography>
+					</UserBox>
 				</Box>
-			</Modal>
+			</StyledModal>
 		</>
 	);
 }
